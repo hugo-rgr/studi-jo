7e0c7b43edc6cd48d96094e319c62b07fe73946d
hugo-rgr <hugo@silk-digital.com>
2024-05-26 20:13:10 +0200

    Add @Controller component in Home Controller

--- src/main/java/com/studi/jo/HomeController.java
+++ src/main/java/com/studi/jo/HomeController.java
@@ -2,9 +2,11 @@ package com.studi.jo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class HomeController {
 
     public static void main(String[] args) {
